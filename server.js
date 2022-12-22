f7b9354a0e56f14d46da0e015e0689ba214cca91
aldoibuado <aldo.ibuado@gmail.com>
2022-12-22 12:53:35 -0800

    added more to the server.js

--- server.js
+++ server.js
@@ -2,12 +2,22 @@
 const path = require("path");
 const express = require("express");
 const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+
+
 
 
 // Setting up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log("Server listening on: http://localhost:" + PORT);
+// will set up Handelbars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+Sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
 });
